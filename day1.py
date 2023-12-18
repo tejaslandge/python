27e7c6572894c86922737e8bf6b73e5a7d28fd7b
Mrunal-Sayre <sayremrunal.5@gmail.com>
2023-12-18 14:19:27 +0530

    updated

---
+++ day1.py
@@ -0,0 +1,41 @@
+import time
+for i in range (1,21):
+    time.sleep(1)
+    if(i==13):
+        #continue
+        print("khatam bye")
+        break
+    print(f"Floor no. {i}")
+
+# n=int  (input("enter a number:"))
+# for i in range(1,11):
+#     print(f"{i}*{n}={i*n} \t {n}*{11-i}={n*(11-i)} ")
+
+
+# for i in range(1,11):
+#     print(f"{i} TEJAS")
+
+# n=int (input("Enter a number:"))
+# if(n%2==1):
+#     print(f"{n} its odd")
+# else:
+#     print(f"{n} its even")
+ 
+# n=int (input("number:"))
+# if(n>0):
+#     print(f"{n} is positive")
+# elif(n<0):
+#     print(f"{n} is negative")
+# else:
+#     print(f"{n} is zero")
+
+#flag=bool(input())
+# if(flag==True):
+#     print("flag  is true")
+# else :
+#     print("flag is false")
+
+#  x=int(input("Enter 1st number:"))
+#  y=int(input("Enter 2nd number:"))
+#  print(f"sum={x+y}")
+
